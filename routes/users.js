e582745be6f62deef6a374928bab4dfbf4f61cd0
Enrico Elizan Jr <eelizanjr@outlook.com>
2022-12-11 19:11:44 -0500

    Users controller and route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  udpateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/userController.js";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", udpateUser);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+router.get("/", getUsers);
 
 export default router;
